348574ca10624e2ba6395119ee5f0c25f0526abd
HellManon <manon.hell@outlook.fr>
2024-10-10 22:52:14 +0200

    ajout du menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome into my mind");
+        System.out.println("choisisser un jeu");
+        System.out.println("choix 1");
+        System.out.println("choix 2");
+
+        //selection du jeu
+        Scanner c = new Scanner(System.in);
+        int choice = c.nextInt();
+
+            switch (choice){
+                case 1 :
+                    //ajout du jeu 2
+                    System.out.println("jeu 1");
+                    break;
+                case 2 :
+                    //ajout du jeu 2
+                    System.out.println("jeu 2");
+                    break;
+                default:
+                    System.out.println("choix inconnu");
+                    break;
+            }
+
     }
 }
